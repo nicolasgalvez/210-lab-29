090271f6f17c69f25a7d6767f210b92c0dfd85e6
Nick Galvez <nicholas.galvez@gmail.com>
2024-11-15 19:55:18 -0800

    get initial queue working

--- src/main.cpp
+++ src/main.cpp
@@ -14,13 +14,29 @@
 #include "Car.h"
 using namespace std;
 
+const int INITIAL_CARS = 2;
+
+/**
+ * Just print a space
+ */
+ostream& s(ostream& os) {
+    os << " ";
+    return os;
+}
+
 int main()
 {
     deque<Car> cars;
+
+cout << "Initial Queue:" << endl;
+    for (size_t i = 0; i < INITIAL_CARS; i++)
+    {
+        cars.push_back(Car());
+        cout << "[" << cars[i].getYear() << s << cars[i].getMake() << s << cars[i].getTransponder() << "] " << "🏎" << endl;
+    }
     
-    // Create initial queue.
-    cars.push_back(Car());
-    cars.push_back(Car());
+
+
 
     
 }
